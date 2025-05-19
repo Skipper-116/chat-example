033faff0123c84eeead52efcb5e3100afdc12860
Roy Chanunkha <roychanunkha@gmail.com>
2025-05-19 13:08:45 -0700

    feat: initialize chat application with Stream Chat and React
    - Added package.json with dependencies for React, Stream Chat, and development tools.
    - Included Vite configuration for development server setup.
    - Created main application structure in App.tsx with chat functionality.
    - Added layout styles for chat components.
    - Defined TypeScript configurations for app and node environments.
    - Included SVG assets for branding.

---
+++ README.md
@@ -0,0 +1,36 @@
+# React + TypeScript + Vite + GetStream
+
+# Getting Started
+
+This project is a simple chat application built with React, TypeScript, and Vite. It uses the GetStream API for real-time messaging and user management.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+    git clone
+    cd chat-example
+   ```
+
+2. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+3. Create a `.env` file in the root directory and add your GetStream API keys:
+   ```env
+   cp .env.example .env
+   ```
+
+## Usage
+
+1. Start the development server:
+
+   ```bash
+   npm run dev
+   ```
+
+2. Open your browser and navigate to `http://localhost:3003`.
+3. To test on mobile, check in the console for the Host Network URL and open it in your mobile browser.
---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import type { Channel as StreamChannel } from "stream-chat";
+import {
+  useCreateChatClient,
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+import "stream-chat-react/dist/css/v2/index.css";
+import "./layout.css";
+
+// your Stream app information
+const apiKey = import.meta.env.VITE_apiKey || "";
+const userId = import.meta.env.VITE_userId || "";
+const userName = import.meta.env.VITE_userName || "";
+const channelId = import.meta.env.VITE_channelId || "";
+const userToken = import.meta.env.VITE_userToken || "";
+
+const App = () => {
+  const [channel, setChannel] = useState<StreamChannel>();
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: { id: userId, name: userName },
+  });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel("medical", channelId);
+
+    setChannel(channel);
+  }, [client]);
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
+  return (
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput minRows={2} maxRows={5} shouldSubmit={() => false} />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default App;
